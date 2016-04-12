ffec8b352fd8da90d3acfca3a6ee13303681dbb9
jc348287 <alfred.susanto@my.jcu.edu.au>
2016-04-12 13:37:17 +0800

    workshop4.2

--- stockPrice.py
+++ stockPrice.py
@@ -7,15 +7,28 @@ MAX_PRICE = 100
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-print("Starting price: ${:,.2f}".format(price))
+
+def format_currency(price):
+    currency= "Starting price: $ {:.2f}".format(price)
+    return currency
+result = format_currency(price)
+print(result)
+
+def format_currency2(price,day):
+    currency2= "On day {} price is: ${:,.2f}".format(day,price)
+    return currency2
+
 day=0
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     priceChange = 0
-    day=day+1
+    day = day + 1
     if random.randint(1, 2) == 1:
         priceChange = random.uniform(0, MAX_INCREASE)
     else:
         priceChange = random.uniform(-MAX_DECREASE, 0)
     price *= (1 + priceChange)
-    print("On day",day,"price is: ${:,.2f}".format(price))
+    result2 = format_currency2(price,day)
+    print(result2)
+
+
