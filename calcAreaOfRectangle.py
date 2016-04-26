16653deab235350e03b6acc37a0b0184863c71bb
jc348287 <alfred.susanto@my.jcu.edu.au>
2016-04-26 12:55:56 +0800

    test

--- calcAreaOfRectangle.py
+++
@@ -1,9 +0,0 @@
-def areaOfRectangle(length,width):
-    area = length*width
-    print("The area of the rectangle: {} m^2".format(area))
-
-length = int(input("Enter length: "))
-width = int(input("Enter width: "))
-
-result= areaOfRectangle(length,width)
-
--- celciusToFahrenheit.py
+++
@@ -1,6 +0,0 @@
-def celciusToFahrenheit(celciusValue):
-    fahrenheitValue = celciusValue * 9/5 + 32
-    print("Temperature today: {}, Fahrenheit: {}".format(celciusValue,fahrenheitValue))
-
-celciusValue=float(input("Enter the temperature today: "))
-result=celciusToFahrenheit(celciusValue)
